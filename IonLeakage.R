7aee0c21cf49541432893335b6309e67647ce5fc
Marijn <marijnknip@gmail.com>
2017-03-28 16:07:52 +0200

    bug-fixing
    found some things that needed to be optimized for the file to be more universally used. Fixed it.

--- IonLeakage.R
+++ IonLeakage.R
@@ -10,22 +10,22 @@ library(rstudioapi)
 
 ##set working directory to location of this .R file and read datafile
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-iondata <- read.table("240217caffeine.csv", ##change to your *.csv
+iondata <- read.table("MylittleData.csv", ##change to your *.csv
                      header = T,
-                     sep = ";",
-                     dec = ","
+                     sep = ",",
+                     dec = "."
 )
 
 ##insert plot title, axis titles, legend text etc. here
 plottitle=    "Rx1-induced ion leakage"
-plotsubtitle= "Coat Protein +/- 25mg/ml Caffeine"
-legendtitle=  "Constucts"
+plotsubtitle= "+/- treatment"
+legendtitle=  "Constructs"
 xaxislabel=   "Time after 20 \U03BCM Dexamethasone treatment (hours)"
 yaxislabel=   "Conductivity (\U03BCS)"
-legendname1=  "CP105"
-legendname2=  "CP105 + Caffeine"
-legendname3=  "CP106"
-legendname4=  "CP106 + Caffeine"
+legendname1=  "Sample1"
+legendname2=  "Sample1+treatment"
+legendname3=  "Sample2"
+legendname4=  "Sample2+treament"
 ##legendname works for 4 entries, change code at p1 to change number
 
 ##summarizing data
@@ -42,7 +42,6 @@ p1 <- ggplot(data, aes(x=Timepoint, y=Conductivity.mean,color= Construct)) +
   geom_point(size=3, fill="white", alpha = 0.4, position = pd) +
   scale_color_brewer(name=legendtitle, 
                      palette="Paired", 
-                     breaks=c("CP105", "CP105+Caffeine", "CP106", "CP106+Caffeine"),
                      labels=c(legendname1, legendname2, legendname3, legendname4)
   )
 p1
@@ -50,18 +49,16 @@ p1
 ##enhancing plot design
 #setting labels and titles
 p2 <- p1 + labs(x=xaxislabel, 
-                y=yaxislabel,
-                title = plottitle,
-                subtitle = plotsubtitle) +
+                y=yaxislabel)+
                 ggtitle(bquote(atop(.(plottitle), atop(.(plotsubtitle), "")))
                )
 p2
 
 #changing Theme
 p3 <- p2 +theme(
-  plot.title = element_text(hjust = 0.5, color="black", size=20, face="bold"),
-  axis.title.x = element_text(color="black", size=16, face="bold"),
-  axis.title.y = element_text(color="black", size=16, face="bold"),
+  plot.title = element_text(hjust = 0.5 , color="black", size=24, face="bold"),
+  axis.title.x = element_text(color="black", size=14, face="bold"),
+  axis.title.y = element_text(color="black", size=14, face="bold"),
   panel.background = element_rect(fill = "#f5f8f1",
                                   colour = "#f5f8f1",
                                   size = 0.5, linetype = "solid"),
@@ -76,9 +73,8 @@ p3 <- p2 +theme(
 )
 p3
 
-
 # save to file
 ggsave(filename = "Ion-leakagePlot.png",plot = p3,width = 7,height = 5,dpi = 600)
-ggsave(filename = "Ion-leakagePlot.pdf",plot = p3,width = 7,height = 5,dpi = 600)
+##ggsave(filename = "Ion-leakagePlot.pdf",plot = p3,width = 7,height = 5,dpi = 600) ##if you want a PDF
 
 
